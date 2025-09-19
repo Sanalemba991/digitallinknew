b92eca0602a8be521d811a8ed0dabe7a9081b765
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-09-19 15:46:40 +0530

    okay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'aos/dist/aos.css';
 import { Toaster } from "react-hot-toast";
-import favicon from '../../public/favicon.svg';
+import favicon from '../../public/favicon.jpg';
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Whatsapp from "./Components/Whatsapp";
